3f261a59ea9853701208497bff1f1080b7cf8196
Benjamin Briggs <bwbriggs1980@gmail.com>
2020-02-03 16:13:17 -0800

    merge conflict

--- src/math/lin/dggt_math_mat.h
+++ src/math/lin/dggt_math_mat.h
@@ -5,55 +5,7 @@
 
 #include "dggt_math_vec.h"
 
-<<<<<<< HEAD
-namespace dggt
-{
-	template <uint32 C,uint32 R,typename T>
-	struct mat
-	{
-		static const uint32 COLUMNS=C;
-		static const uint32 ROWS=R;
 
-		T e[C*R];
-
-		mat(const T e[C*R]);
-		mat(const mat& other);
-		mat();
-
-		mat operator+(const mat& rhs) const;
-		mat operator-(const mat& rhs) const;
-		mat operator*(const T& scalar) const;
-		template <uint32 M>
-		mat<C,M,T> operator*(const mat<R,M,T>& rhs) const;
-
-		mat& operator=(const mat& rhs);
-		mat& operator+=(const mat& rhs);
-		mat& operator-=(const mat& rhs);
-		mat& operator*=(const T& scalar);
-		template <uint32 M>
-		mat<C,M,T>& operator*=(const mat<R,M,T>& rhs);
-
-		vec<R,T> transform(const vec<C,T>& vector) const;
-
-		T* data() { return e; }
-		const T* data() const { return e; }
-
-		mat<R,C,T> transpose() const;
-
-		T& get(uint32 column,uint32 row);
-		const T& get(uint32 column,uint32 row) const;
-		vec<R,T> get_column(uint32 index) const;
-		vec<C,T> get_row(uint32 index) const;
-		mat& set_column(uint32 column,const vec<R,T>& val);
-		mat& set_row(uint32 row,const vec<C,T>& val);
-	};
-
-	template <uint32 C,uint32 R,typename T>
-	vec<R,T> operator*(const vec<C,T>& vector,const mat<C,R,T>& matrix);
-	template <uint32 C,uint32 R,typename T>
-	mat<C,R,T> operator*(const T& scalar,const mat<C,R,T>& matrix);
-}
-=======
 /*!
  * @brief A matrix struct with C columns and R rows of elements of type T.
  * @tparam C An integer representing the number of columns.
@@ -298,7 +250,6 @@ vec<R,T> operator*(const vec<C,T>& vector,const mat<C,R,T>& matrix);
 template <uint32 C,uint32 R,typename T>
 mat<C,R,T> operator*(const T& scalar,const mat<C,R,T>& matrix);
 
->>>>>>> docs
 #include "dggt_math_mat.inl"
 
 /*! @cond IncludeGuard */
--- src/math/lin/dggt_math_vec.h
+++ src/math/lin/dggt_math_vec.h
@@ -7,9 +7,6 @@
 namespace dggt
 {
 	/*!
-<<<<<<< HEAD
-	 * @brief A vector struct of dimension, D and type T.
-=======
 	 * @brief A static const unsigned integer representing the vector's dimension.
 	 * */
 	static const uint32 DIM=D;
@@ -37,7 +34,6 @@ namespace dggt
 	vec(const vec& other);
 	/*!
 	 * @brief A constructor for instantiating vectors from a vector of a dimension one less than D.
->>>>>>> docs
 	 *
 	 * A vector is a mathematical construct consisting of an ordered list of elements along with operations on it.
 	 * @tparam D An integer representing the dimension of the vector. (ie. The number of elements in the ordered list)
@@ -55,90 +51,6 @@ namespace dggt
 		 * */
 		T e[D];
 
-<<<<<<< HEAD
-		/*!
-		 * @brief Default constructor.
-		 *
-		 * Initializes all elements to T(0).
-		 * */
-		vec();
-		/*!
-		 * @brief An explicit constructor which initializes every element to a given value.
-		 * @param val The value each element will be initialized to.
-		 * @note This constructor is explicit so one can't simply write something like, vec<2,float32> v=5.0f, a seemingly nonsensical and confusing mathematical statement.
-		 * */
-		explicit vec(const T& val);
-		/*!
-		 * @brief Basic copy constructor.  Will copy every element in a given vector.
-		 * @param other A vector of the same type and dimension to copy.
-		 * */
-		vec(const vec& other);
-		/*!
-		 * @brief A constructor for instantiating vectors from a vector of a dimension one less than D.
-		 *
-		 * One occasionally needs to instatiate a vector from a vector of a lower dimension.  The last element will default to one to be in accordance with homogeneous coordinates used in rendering. 
-		 * @param other A vector with a dimension one less than the instantiated vector.
-		 * @param last The last element in the new vector.  Defaults to T(1).
-		 * */
-		vec(const vec<D-1,T>& other, T last=T(1));
-		/*!
-		 * @brief A copy constructor for instantiating vectors from a vector of any other dimension.
-		 * @tparam D2 The dimension of the other vector.
-		 * @param other A vector of any other dimension.  If D2 is less than D then it will copy everything in other, followed by setting further elements to T(0)
-		 * except for the last element which will be set to T(1).  If D2 is greater than D then everything that can be copied will effectively truncating the vector.
-		 * */
-		template <uint32 D2>
-			vec(const vec<D2,T>& other);
-
-		/*!
-		 * @brief Addition operator.
-		 *
-		 * Adds two vectors by adding each corresponding element.
-		 * @param rhs The right hand side of the '+' symbol.
-		 * @return A vector where every element is the sum of the corresponding elements in the vector sum.
-		 * */
-		vec operator+(const vec& rhs) const;
-		/*!
-		 * @brief Subtraction operator.
-		 *
-		 * Subtracts two vectors by subtracting each corresponding element.
-		 * @param rhs The right hand side of the '-' symbol.
-		 * @return A vector where every element is the difference of the corresponding elements in the vector difference.
-		 * */
-		vec operator-(const vec& rhs) const;
-		/*!
-		 * @brief Unary negation operator.
-		 *
-		 * Negates a vector by negating every element resulting in a vector pointing in the opposite direction.
-		 * @return The negative of the vector.
-		 * */
-		vec operator-() const;
-		vec operator*(const T& scalar) const;
-		vec operator/(const T& scalar) const;
-		bool32 operator==(const vec& rhs) const;
-		bool32 operator!=(const vec& rhs) const;
-		vec& operator=(const vec& rhs);
-		vec& operator-=(const vec& rhs);
-		vec& operator+=(const vec& rhs);
-		vec& operator*=(const T& scalar);
-		vec& operator/=(const T& scalar);
-
-		T& operator[](uint32 index);
-		const T& operator[](uint32 index) const;
-
-		T dot(const vec& rhs) const;
-
-		T* data() { return e; }
-		const T* data() const {return e; }
-	};
-
-	template <uint32 D, typename T>
-	vec<D,T> operator*(const T& scalar,const vec<D,T>& vector);
-
-	template <uint32 D, typename T>
-	T dot(const vec<D,T>& lhs,const vec<D,T>& rhs);
-}
-=======
 	/*!
 	 * @brief Addition operator.
 	 *
@@ -316,7 +228,6 @@ vec<D,T> operator*(const T& scalar,const vec<D,T>& vector);
  * */
 template <uint32 D, typename T>
 T dot(const vec<D,T>& lhs,const vec<D,T>& rhs);
->>>>>>> docs
 
 #include "dggt_math_vec.inl"
 
