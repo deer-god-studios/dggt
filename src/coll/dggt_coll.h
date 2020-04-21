6fb965d0326169565550c8351ff65eecb78d4226
Benjamin Briggs <bwbriggs1980@gmail.com>
2020-04-20 23:22:18 -0700

    ..

--- src/coll/dggt_coll.h
+++ src/coll/dggt_coll.h
@@ -4,16 +4,15 @@
 #include "dggt_coll_sllist.h"
 #include "dggt_coll_starray.h"
 #include "dggt_coll_dllist.h"
-/*
+/* // TODO: test(!!)
 #include "dggt_coll_stack.h"
 #include "dggt_coll_queue.h"
-#include "dggt_coll_idxtable.h"
 #include "dggt_coll_pgelist.h"
-#include "dggt_coll_bintree.h"
-#include "dggt_coll_heap.h"
-#include "dggt_coll_graph.h"
 #include "dggt_coll_chntable.h"
 #include "dggt_coll_oatable.h"
+//#include "dggt_coll_bintree.h"
+#include "dggt_coll_heap.h"
+#include "dggt_coll_graph.h"
 */
 
 #define _DGGT_COLL_H_
